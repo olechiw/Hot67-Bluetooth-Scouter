0a188befe62957023f278968396e5f843d5d0f6e
pianist1119 <pianist1119@gmail.com>
2017-03-12 15:32:16 -0400

    Finished first version of custom gridlayout. Untested.

--- app/src/main/java/org/hotteam67/bluetoothscouter/ScoutGridLayout.java
+++ app/src/main/java/org/hotteam67/bluetoothscouter/ScoutGridLayout.java
@@ -2,6 +2,8 @@ package org.hotteam67.bluetoothscouter;
 
 import android.app.Activity;
 import android.support.v4.widget.TextViewCompat;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.content.Context;
 import android.view.View;
@@ -13,6 +15,7 @@ import android.widget.LinearLayout;
 import android.view.Gravity;
 import java.util.*;
 import android.util.Log;
+import android.util.DisplayMetrics;
 
 
 /**
@@ -42,7 +45,63 @@ public class ScoutGridLayout extends GridLayout
             if (v != null)
                 views.add(v);
             else
-                l("Null returned on intializeView. Tag: " + d.getKey() + " Type: " + d.getValue());
+                l("Null returned on intializeView(). Tag: " + d.getKey() + " Type: " + d.getValue());
+        }
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        ((Activity)getContext()).getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int width = metrics.widthPixels;
+
+        int i = 0;
+        int rows = 0;
+        List<View> rowViews;
+        while (i < views.size())
+        {
+            rowViews = new ArrayList<>();
+            int w = width;
+            int h = 0;
+
+
+            w -= measureCellWidth(getContext(), views.get(i));
+            rowViews.add(views.get(i));
+
+            h = (measureCellHeight(getContext(), views.get(i)) > h) ?
+                    measureCellHeight(getContext(), views.get(i)) : h;
+
+
+            while (i < views.size() && w >= 0)
+            {
+                ++i;
+                w -= measureCellWidth(getContext(), views.get(i));
+                if (w >= 0)
+                {
+                    rowViews.add(views.get(i));
+                    h = (measureCellHeight(getContext(), views.get(i)) > h) ?
+                            measureCellHeight(getContext(), views.get(i)) : h;
+                }
+            }
+
+            AddRow(rows, h, rowViews);
+
+            ++i;
+            ++rows;
+        }
+    }
+
+    private void AddRow(int rowNumber, int rowHeight, List<View> views)
+    {
+        int i = 0;
+        for (View v : views)
+        {
+            Spec column = spec(i);
+            Spec row = spec(rowNumber);
+            LayoutParams params = new LayoutParams(row, column);
+            params.height = rowHeight;
+            params.width = measureCellWidth(getContext(), v);
+            params.setGravity(Gravity.CENTER);
+            v.setLayoutParams(params);
+            addView(v);
+            ++i;
         }
     }
 
@@ -53,36 +112,92 @@ public class ScoutGridLayout extends GridLayout
         {
             case TYPE_BOOLEAN:
                 v = new CheckBox(getContext());
-                ((CheckBox)v).setText(tag);
+                ((CheckBox) v).setText(tag);
                 break;
             case TYPE_STRING:
-                v = ((Activity)getContext()).getLayoutInflater().inflate(R.layout.layout_edittext, null);
-                ((TextView)v.findViewById(R.id.textLabel)).setText(tag);
+                v = getInflater().inflate(R.layout.layout_edittext, null);
+                ((TextView) v.findViewById(R.id.textLabel)).setText(tag);
 
                 TextViewCompat.setTextAppearance(
-                        ((TextView)v.findViewById(R.id.textLabel)),
+                        ((TextView) v.findViewById(R.id.textLabel)),
                         android.R.style.TextAppearance_DeviceDefault);
                 break;
             case TYPE_INTEGER:
-                v = ((Activity)getContext()).getLayoutInflater().inflate(R.layout.layout_numberpicker, null);
-                ((TextView)v.findViewById(R.id.numberLabel)).setText(tag);
+                v = getInflater().inflate(R.layout.layout_numberpicker, null);
+                ((TextView) v.findViewById(R.id.numberLabel)).setText(tag);
 
                 TextViewCompat.setTextAppearance(
-                        ((TextView)v.findViewById(R.id.numberLabel)),
+                        ((TextView) v.findViewById(R.id.numberLabel)),
                         android.R.style.TextAppearance_DeviceDefault);
 
 
-                ((NumberPicker)v.findViewById(R.id.numberPicker)).setMinValue(0);
-                ((NumberPicker)v.findViewById(R.id.numberPicker)).setMaxValue(100);
+                ((NumberPicker) v.findViewById(R.id.numberPicker)).setMinValue(0);
+                ((NumberPicker) v.findViewById(R.id.numberPicker)).setMaxValue(100);
                 break;
             case TYPE_HEADER:
-
+                v = getInflater().inflate(R.layout.layout_header, null);
         }
+
+        if (v != null)
+        {
+            v.setTag(R.string.value_name, tag);
+            v.setTag(R.string.value_type, type);
+        }
+
         return v;
     }
 
     private void l(String s)
     {
         Log.d(ScoutActivity.TAG, s);
+    }
+
+    private LayoutInflater getInflater()
+    {
+        return ((Activity)getContext()).getLayoutInflater();
+    }
+
+
+    private int measureCellWidth( Context context, View cell )
+    {
+
+        // We need a fake parent
+        FrameLayout buffer = new FrameLayout( context );
+        android.widget.AbsListView.LayoutParams layoutParams
+                = new  android.widget.AbsListView.LayoutParams(
+                android.widget.AbsListView.LayoutParams.WRAP_CONTENT,
+                android.widget.AbsListView.LayoutParams.WRAP_CONTENT);
+
+        buffer.addView( cell, layoutParams);
+
+        cell.forceLayout();
+        cell.measure(1000, 1000);
+
+        int width = cell.getMeasuredWidth();
+
+        buffer.removeAllViews();
+
+        return width;
+    }
+    private int measureCellHeight( Context context, View cell )
+    {
+
+        // We need a fake parent
+        FrameLayout buffer = new FrameLayout( context );
+        android.widget.AbsListView.LayoutParams layoutParams
+                = new  android.widget.AbsListView.LayoutParams(
+                android.widget.AbsListView.LayoutParams.WRAP_CONTENT,
+                android.widget.AbsListView.LayoutParams.WRAP_CONTENT);
+
+        buffer.addView( cell, layoutParams);
+
+        cell.forceLayout();
+        cell.measure(1000, 1000);
+
+        int height = cell.getMeasuredHeight();
+
+        buffer.removeAllViews();
+
+        return height;
     }
 }
