ae9586ec148d9b76bec8acfb708c6f4b66220f2c
pianist1119 <pianist1119@gmail.com>
2017-04-15 14:49:09 -0400

    About to make the move to using filehandler

--- app/src/main/java/org/hotteam67/bluetoothscouter/FileHandler.java
+++ app/src/main/java/org/hotteam67/bluetoothscouter/FileHandler.java
@@ -2,6 +2,10 @@ package org.hotteam67.bluetoothscouter;
 
 import android.os.Environment;
 
+import java.io.*;
+import java.util.*;
+import android.util.Log;
+
 
 /**
  * Created by Jakob on 3/26/2017.
@@ -11,30 +15,138 @@ public final class FileHandler
 {
     public static final String SERVER_FILE = "database.csv";
     public static final String SCOUTER_FILE = "schema.csv";
+    public static final String MATCHES_FILE = "matches.csv";
     public static final String DIRECTORY =
             Environment.getExternalStorageDirectory().getAbsolutePath() + "/BluetoothScouter/";
 
 
-
     public static final int SERVER = 1;
     public static final int SCOUTER = 2;
+    public static final int MATCHES = 3;
 
 
-    public static final String LoadContents(int FILE)
+    private static  String file(int FILE)
     {
+        String f = DIRECTORY;
         switch (FILE)
         {
             case SERVER:
-                return LoadContents(SERVER_FILE + DIRECTORY);
+                f += SERVER_FILE;
+                break;
             case SCOUTER:
-                return LoadContents(SCOUTER_FILE + DIRECTORY);
+                f += SCOUTER_FILE;
+                break;
+            case MATCHES:
+                f += MATCHES_FILE;
+                break;
             default:
-                return "";
+                return null;
+        }
+        return f;
+    }
+
+    private static final BufferedReader GetReader(int FILE)
+    {
+        String f = file(FILE);
+
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(f));
+            return reader;
+        }
+        catch (FileNotFoundException e)
+        {
+            l("File not found");
+            new File(DIRECTORY).mkdirs();
+
+            try
+            {
+                new File(f).createNewFile();
+                return new BufferedReader(new FileReader(f));
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        catch (Exception e)
+        {
+            l("Exception occured in loading reader : " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    private static final BufferedWriter GetWriter(int FILE)
+    {
+        String f = file(FILE);
+
+        try
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(f));
+            return writer;
+        }
+        catch (FileNotFoundException e)
+        {
+            l("File not found");
+            new File(DIRECTORY).mkdirs();
+
+            try
+            {
+                new File(f).createNewFile();
+                return new BufferedWriter(new FileWriter(f));
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        catch (Exception e)
+        {
+            l("Exception occured in loading reader : " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    private static final String LoadContents(int file)
+    {
+        String content = "";
+        BufferedReader r = GetReader(file);
+        try
+        {
+            String line = r.readLine();
+            while (line != null)
+            {
+                content += line + "\n";
+            }
+        }
+        catch (Exception e)
+        {
+            l("Failed to load :" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return content;
+    }
+
+    public static final void Write(int FILE, String s)
+    {
+        try
+        {
+            BufferedWriter w = GetWriter(FILE);
+            w.write(s);
+            w.close();
+        }
+        catch (Exception e)
+        {
+            l("Failed to write: " + s + ": " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    private static final String LoadContents(String file)
+    private static final void l(String s)
     {
-        return "";
+        Log.d("[File Handling]", s);
     }
 }
