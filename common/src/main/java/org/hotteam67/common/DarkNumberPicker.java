98005d66a7ad83d438e19f4c8e1fa631b917393a
Jakob <pianist1119@gmail.com>
2018-02-03 15:13:15 -0500

    Fixed limits on numberpicker

--- common/src/main/java/org/hotteam67/common/DarkNumberPicker.java
+++ common/src/main/java/org/hotteam67/common/DarkNumberPicker.java
@@ -1,8 +1,10 @@
 package org.hotteam67.common;
 
+import android.text.Editable;
 import android.text.InputFilter;
 import android.text.InputType;
 import android.text.Spanned;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,25 +47,69 @@ public class DarkNumberPicker extends LinearLayout
             @Override
             public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend)
             {
-                int i = 0;
+                int i;
                 try
                 {
                     i = Integer.valueOf(String.valueOf(source.toString()));
                 } catch (Exception e) {
-                    //android.util.Log.e("[DarkPicker]", "Failed to create integer value of : " + source.toString(), e);
+                    return String.valueOf(minimum);
                 }
 
                 //android.util.Log.d("[DarkPicker]", "Integer value: " + i);
 
                 if ((i >= minimum && i <= maximum))
-                    return null;
-                else
+                    return String.valueOf(i);
+                else if (i < minimum)
                 {
                     return String.valueOf(minimum);
                 }
+                else if (i > maximum)
+                {
+                    return String.valueOf(maximum);
+                }
+                else
+                    return String.valueOf(minimum);
             }
         };
         mainText.setFilters(new InputFilter[] { filter });
+
+        mainText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int i = 0;
+                try
+                {
+                    i = Integer.valueOf(String.valueOf(mainText.getText().toString()));
+                } catch (Exception e) {
+                    mainText.setText(String.valueOf(minimum));
+                }
+
+                //android.util.Log.d("[DarkPicker]", "Integer value: " + i);
+
+                if ((i >= minimum && i <= maximum)) {
+                }
+                else if (i < minimum)
+                {
+                    mainText.setText(String.valueOf(minimum));
+                }
+                else if (i > maximum)
+                {
+                    mainText.setText(String.valueOf(maximum));
+                }
+                else
+                    mainText.setText(String.valueOf(minimum));
+            }
+        });
 
         LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         params.gravity = Gravity.CENTER_HORIZONTAL;
