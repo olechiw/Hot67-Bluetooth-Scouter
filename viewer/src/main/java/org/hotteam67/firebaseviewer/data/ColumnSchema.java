51c2397c065d47d9af66cc79ad6f3c08b2ca353f
Olechiw <jakob.olechiw@gmail.com>
2019-05-01 13:28:53 -0400

    Column schema explanation

--- viewer/src/main/java/org/hotteam67/firebaseviewer/data/ColumnSchema.java
+++ viewer/src/main/java/org/hotteam67/firebaseviewer/data/ColumnSchema.java
@@ -7,13 +7,20 @@ import java.util.List;
 import java.util.function.Function;
 
 /**
- * The viewer schema for columns, adds a preferred order for tags found in the JSON pulled from
- * Firebase
+ * The Viewer schema, should be updated to match the schema created in the Server app (or written manually for the brave)
+ *
+ * Need to update all three - sumcolumns, calculated columns, and preferred order. FIRST do sumc olumns, THEN preferred order,
+ * THEN calculated columns
+ *
+ * A note about "raw names" - they are either the name as named in SumColumns, or the name as named in the schema.
+ * If the raw name is from the schema it has the most recent header first, such as "Teleop Hatch Panels".
+ * If the raw name is from SumColumns(), you obviously need to add a SumColumns() entry, then you can use calculated columns on that
  */
-
 public class ColumnSchema {
+
     /**
-     * The calculated columns desired names, in the same order as the raw names
+     * The calculated columns desired names, first argument is name as it appears in the viewer, second is the raw name
+     * @return the calculated columns to show in the main view
      */
     public static List<CalculatedColumn> CalculatedColumns() {
         List<CalculatedColumn> calculatedColumns = new ArrayList<>();
@@ -40,6 +47,14 @@ public class ColumnSchema {
         return calculatedColumns;
     }
 
+    /**
+     * Populate this with all of the various "raw names" either from SumColumns() or the schema.
+     * THIS NEEDS TO BE DONE FOR THE CALCULATED COLUMNS - so if you have a calculated column "To. Pieces" then "Total Pieces"
+     * needs to be somewhere in here. This is a quirk of using arbitrarily ordered JSON that I never properly fixed.
+     *
+     * The actual functionality of this list of names is that it determines the order in which these columns appear in raw data
+     * @return A list of raw column names which determines the order they appear in the viewer's raw data view
+     */
     public static List<String> PreferredOrder()
     {
         return new ArrayList<>(Arrays.asList(
@@ -68,8 +83,10 @@ public class ColumnSchema {
     }
 
     /**
-     * List of columns to sum, with a special class, before doing maximum/average calcs
+     * List of columns to sum, will add a "raw column" for each match scouted with the new calculated value. You might be
+     * able to use sum columns in other sum columns, but it is definitely SAFER TO JUST WRITE THEM ALL MANUALLY.
      *
+     * Here this is accomplished by using addAll from the other sumcolumns, rather than rewriting everything.
      * @return List of sumcolumns
      */
     public static List<SumColumn> SumColumns() {
@@ -116,10 +133,21 @@ public class ColumnSchema {
         return sumColumns;
     }
 
+    /**
+     * Utility macro
+     * @param addTo list to populate
+     * @param values variable argument array, makes writing this easier
+     */
     private static void addAll(List<SumColumn> addTo, SumColumn... values) {
         addTo.addAll(Arrays.asList(values));
     }
 
+    /**
+     * Another simple builder
+     * @param name "raw name" of the sumcolumn
+     * @param columns variable argument array, with the actual other raw names to sum
+     * @return SumColumn
+     */
     private static SumColumn BuildSumColumn(String name, String... columns) {
         SumColumn column = new SumColumn();
         column.columnName = name;
